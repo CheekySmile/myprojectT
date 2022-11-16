3b272ca1ee186a028d4ac9839d12c3d8d4dd9600
Vladislav Ivonin <vladivonin@gmail.com>
2022-11-16 22:11:39 +0530

    Fixed base_page.py Update elements_test.py added test checkboxes

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,13 +1,12 @@
 import time
 
 from pages.base_page import BasePage
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
 
 class TestElements:
     class TestTextBox:
-
-        def test_text_box(self, driver):
+        def test_text_box(self, driver):        # проверяем текстбокс
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
             full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
@@ -16,3 +15,16 @@ class TestElements:
             assert email == output_email, "the email does not match"
             assert current_address == output_cur_addr, "the current address does not match"
             assert permanent_address == output_per_addr, "the permanent does not match"
+
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            assert input_checkbox == output_result, "checkboxes have not been selected"
+
+
+
