edae514141153e900db6b31b92f23be992fa7fc0
Vladislav Ivonin <vladivonin@gmail.com>
2022-11-17 01:54:36 +0530

    Update elements_test.py added test web table add person, test web table search person

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,7 +1,8 @@
+import random
 import time
 
 from pages.base_page import BasePage
-from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage
 
 
 class TestElements:
@@ -39,6 +40,22 @@ class TestElements:
             assert output_yes == 'Yes', "'Yes' have not been selected"
             assert output_impressive == 'Impressive', "'Impressive' have not been selected"
             assert output_no == 'No', "'No' have not been selected"
+
+    class TestWebTable:
+        def test_web_table_add_person(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            new_person = web_table_page.add_new_person()
+            check_person = web_table_page.check_new_person()
+            assert new_person in check_person, "Person does not exist"
+
+        def test_web_table_search_person(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            keyword = web_table_page.add_new_person()[random.randint(0, 5)]
+            web_table_page.search_person(keyword)
+            table_result = web_table_page.check_search_person()
+            assert keyword in table_result, "Person not found"
 
 
 
